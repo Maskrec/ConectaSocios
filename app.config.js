ea3aa98efd9dd3ae9533ec2f3733a61c3a8d80fc
Maskrec <alanmaerin@gmail.com>
2026-08-20 11:31:55 -0600

    chore: bump application version to 2.2.8 and pin React Native Firebase dependencies

--- app.config.js
+++ app.config.js
@@ -2,7 +2,7 @@ export default {
   expo: {
     name: "Conecta-socios",
     slug: "app-socios",
-    version: "2.2.4",
+    version: "2.2.8",
     orientation: "portrait",
     icon: "./assets/icon.png",
     userInterfaceStyle: "light",
@@ -20,7 +20,7 @@ export default {
       }
     },
     android: {
-      versionCode: 224,
+      versionCode: 228,
       adaptiveIcon: {
         foregroundImage: "./assets/adaptive-icon.png",
         backgroundColor: "#ffffff"
